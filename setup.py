e448db1df2badb06daf40a892e3845a40cd8d6a5
EGOR KONYAKHIN <atomic14@mail.ru>
2023-04-24 17:12:16 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='RawImagesReader',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Egor Konyakhin',
+    author_email='atomic14@mail.ru',
+    description='The program reads raw images from directory and display them'
+)
